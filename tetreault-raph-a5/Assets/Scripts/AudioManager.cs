5673581ac1a0481c68db4417112e9ff590ed0663
JoshSimonMCGD <josh.j.simon@gmail.com>
2025-04-15 21:13:15 -0400

    Add SFX scripts
    Added the beginning of an audio manager and sfx scripts for all game actions.

---
+++ tetreault-raph-a5/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("Audio Sources")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource sfxSource;
+
+    [Header("Audio Clips")]
+    public AudioClip Lazer;
+    public AudioClip Death;
+    public AudioClip MeteorBreak;
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+}
